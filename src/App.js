7f3d284390a9287ade5c75c6db744abf9e3abdc6
Aytaj-Ahmadli <aytacahmadli6@gmail.com>
2023-06-10 16:27:23 +0400

    add change

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
 function App() {
+
   return (
-    <div className="App">
-        <h1>hello</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage/>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/redux/reducers/categoriesReducer.js
@@ -0,0 +1,39 @@
+import actionTypes from "../actions/actionTypes";
+
+const initialState={
+  pending:false,
+  success:false,
+  categories:[],
+  fail:false,
+  error:""
+}
+
+const catergoriesReducer=(state=initialState,action)=>{
+  switch (action.type) {
+    case actionTypes.categoryActions.GET_CATEGORIES_START:
+      return{
+        ...state,
+        pending:true
+      }
+      case actionTypes.categoryActions.GET_CATEGORIES_SUCCESS:
+        return{
+          ...state,
+          pending:false,
+          success:true,
+          categories:action.payload,
+          fail:false
+        }
+        case actionTypes.categoryActions.GET_CATEGORIES_FAIL:
+            return{
+              ...state,
+              pending:false,
+              success:false,
+              fail:true,
+              error:action.payload
+            }
+    default:
+      return state
+
+  }
+}
+  export default catergoriesReducer;
